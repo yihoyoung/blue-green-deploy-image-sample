307c9cedbb8f7a17c74471d8d7f4d2eb4e1a9965
yihoyoung <yihoyoung@nate.com>
2019-01-12 23:55:44 +0800

    add sleep time

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -11,6 +11,13 @@ public class GreetingController {
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
+        try {
+            System.out.print("Strart sleep 8 seconds...");
+            Thread.sleep(8000);
+        } catch (Exception e) {
+
+            System.out.print(e.getMessage());
+        }
         return "greeting";
     }
 
