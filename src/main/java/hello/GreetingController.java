31a7be35b60037880971335c02b184137b92cd1f
yihoyoung <yihoyoung@nate.com>
2019-01-13 17:24:24 +0800

    add sleep time

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -7,13 +7,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingController {
+    final int SLEEP_TIME = 20000;
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         try {
-            System.out.print("Strart sleep 8 seconds...");
-            Thread.sleep(8000);
+            System.out.print("Strart sleep " + SLEEP_TIME/1000 + " seconds...");
+            Thread.sleep(SLEEP_TIME);
         } catch (Exception e) {
 
             System.out.print(e.getMessage());
